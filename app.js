ea932f05f72998d9c3fbe7280e63275623d8ebf8
jrodriguez1214 <joshuar@squareup.com>
2026-04-13 19:04:44 -0400

    Thin elegant Hearthstone brand title on dashboard

--- app.js
+++ app.js
@@ -747,6 +747,7 @@ Pages.dashboard = {
 
     $('page-root').innerHTML = `
       <div class="hub-layout">
+        <h1 class="hub-brand">Hearthstone</h1>
         <div class="hub-list">
           ${tilesHTML}
         </div>
